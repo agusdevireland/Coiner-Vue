2c9dbacdfa2d7f845ce2df11f91518af96ff58c0
Agustin-Graciano <agus-dev-ireland@hotmail.com>
2024-11-06 00:29:20 +0000

    Fourth Commit
    Setting up the Chrome extension and files to fetch some data as a test

---
+++ src/components/PopUp.vue
@@ -0,0 +1,34 @@
+<!-- src/popup/PopupApp.vue -->
+<template>
+    <div class="p-4">
+      <h1 class="text-xl font-bold text-center">Crypto Extension</h1>
+      <button @click="fetchCryptoData" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded">
+        Fetch Crypto Data
+      </button>
+      <div v-if="cryptoData" class="mt-4">
+        <p>Bitcoin Price: ${{ cryptoData.bitcoin.usd }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const cryptoData = ref(null);
+  
+      async function fetchCryptoData() {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+        cryptoData.value = await response.json();
+      }
+  
+      return { cryptoData, fetchCryptoData };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Additional styles */
+  </style>
+  
